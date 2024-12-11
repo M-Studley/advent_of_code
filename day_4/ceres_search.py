593c07ef06021605342a9d100a3d40685e656a05
Michael Studley <michael.r.studley@gmail.com>
2024-12-11 13:20:21 +0400

    ceres search part 1

--- day_4/ceres_search.py
+++ day_4/ceres_search.py
@@ -1,16 +1,17 @@
 # 18 matches
 grid = [
-['M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M'],  # 0
-['M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A'],  # 1
-['A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M'],  # 2
-['M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X'],  # 3
-['X', 'M', 'A', 'S', 'A', 'M', 'X', 'A', 'M', 'M'],  # 4
-['X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A'],  # 5
-['S', 'M', 'S', 'M', 'S', 'A', 'S', 'X', 'S', 'S'],  # 6
-['S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A'],  # 7
-['M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M'],  # 8
-['M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X']   # 9
+    ['M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M'],  # 0
+    ['M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A'],  # 1
+    ['A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M'],  # 2
+    ['M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X'],  # 3
+    ['X', 'M', 'A', 'S', 'A', 'M', 'X', 'A', 'M', 'M'],  # 4
+    ['X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A'],  # 5
+    ['S', 'M', 'S', 'M', 'S', 'A', 'S', 'X', 'S', 'S'],  # 6
+    ['S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A'],  # 7
+    ['M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M'],  # 8
+    ['M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X']  # 9
 ]
+
 
 def search_direction(data: list[list[str]], key_word: str, r_idx: int, c_idx: int, row_step: int, col_step: int) -> int:
     matches = 0
@@ -41,13 +42,13 @@ for row_idx, row in enumerate(grid):
     for col_idx, col in enumerate(row):
         if col == 'X':
             total += search_direction(grid, 'XMAS', row_idx, col_idx, 0, 1)  # Right
-            total += search_direction(grid, 'XMAS', row_idx, col_idx, 0, -1) # Left
+            total += search_direction(grid, 'XMAS', row_idx, col_idx, 0, -1)  # Left
             total += search_direction(grid, 'XMAS', row_idx, col_idx, 1, 0)  # Down
-            total += search_direction(grid, 'XMAS', row_idx, col_idx, -1, 0) # Up
+            total += search_direction(grid, 'XMAS', row_idx, col_idx, -1, 0)  # Up
 
-            total += search_direction(grid, 'XMAS', row_idx, col_idx, 1, 1) # Down Right
-            total += search_direction(grid, 'XMAS', row_idx, col_idx, 1, -1) # Down Left
-            total += search_direction(grid, 'XMAS', row_idx, col_idx, -1, 1) # Up Right
-            total += search_direction(grid, 'XMAS', row_idx, col_idx, -1, -1) # Up Left
+            total += search_direction(grid, 'XMAS', row_idx, col_idx, 1, 1)  # Down Right
+            total += search_direction(grid, 'XMAS', row_idx, col_idx, 1, -1)  # Down Left
+            total += search_direction(grid, 'XMAS', row_idx, col_idx, -1, 1)  # Up Right
+            total += search_direction(grid, 'XMAS', row_idx, col_idx, -1, -1)  # Up Left
 
 print(total)
